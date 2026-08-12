c3cfc3c58e3e3aec756772fb91a0b1e3c07eff7b
Shohabdukarimov <abdukarimovshohruhbek5@gmail.com>
2026-08-12 15:19:49 +0500

    app.py

--- app.py
+++ app.py
@@ -39,5 +39,8 @@ def click():
 def index():
     return render_template('index.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
